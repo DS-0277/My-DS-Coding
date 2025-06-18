09ea5c624f20b6861491304be7e77bcb74c0ff3d
Dara Sen <darasen0277@gmail.com>
2025-06-17 18:20:12 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,11 @@
-player_choice = "rock"
-computer_choice = "paper"
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+    
 
-print(player_choice)
-print(computer_choice)
+choices = get_choices()
+print(choices)
+
+dict = {"name": "beau", "color": choices}
